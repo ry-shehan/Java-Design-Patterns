c52a673769f378240f099a10fe1c43a2155d3d69
ry-shehan <ry.shehan.dev@gmail.com>
2025-08-11 22:26:42 +0530

    implement thread-safe singleton Dog class with double-checked locking

---
+++ src/main/java/org/shehan/ry/Dog.java
@@ -0,0 +1,28 @@
+package org.shehan.ry;
+
+public class Dog {
+
+    // 'volatile' ensures that changes to 'dog' are immediately visible to all threads,
+    // preventing problems with threads seeing a partially created instance.
+
+    private static volatile Dog dog;
+
+    private Dog() {
+    }
+
+    public void barking() {
+        System.out.println("Wof... Wof...");
+    }
+
+    public static Dog getInstance() {
+        if (dog == null) {
+            synchronized (Dog.class) {
+                if (dog == null) {
+                    dog = new Dog();
+                }
+            }
+        }
+        return dog;
+    }
+
+}
